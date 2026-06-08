452058cf5ca0a9d7133187610001fbf744d8acef
JasonShin <visualbbasic@gmail.com>
2026-06-08 16:43:38 +1000

    Initial commit

---
+++ week0/numpy_test.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+
+def main():
+    n = int(input("enter your number: "))
+    matrix = np.random.randint(0, 100, (n, n))
+
+    print("Generated Matrix:")
+    print(matrix)
+    print("\nSum of each row:", np.sum(matrix, axis=1))
+    print("Sum of each column:", np.sum(matrix, axis=0))
+    print("Overall sum:", np.sum(matrix))
+
+if __name__ == "__main__":
+    main()
